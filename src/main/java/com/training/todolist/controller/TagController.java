fbef566bddba80bdffed2bb2f1edecaaaa358f8c
Ernest Cheung <ernestcheung@trustmechain.com>
2020-12-11 00:09:42 +0800

    [TagController][Ernest] Feat: used tag mapper and request and response

--- src/main/java/com/training/todolist/controller/TagController.java
+++ src/main/java/com/training/todolist/controller/TagController.java
@@ -1,10 +1,15 @@
 package com.training.todolist.controller;
 
+import com.training.todolist.dto.TagRequest;
+import com.training.todolist.dto.TagResponse;
 import com.training.todolist.entity.Tag;
 import com.training.todolist.entity.Todo;
 import com.training.todolist.exceptions.TagNotFoundException;
 import com.training.todolist.exceptions.TodoNotFoundException;
+import com.training.todolist.mapper.TagMapper;
+import com.training.todolist.mapper.TodoMapper;
 import com.training.todolist.services.TagService;
+import com.training.todolist.services.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -14,8 +19,13 @@ import java.util.List;
 @RestController
 @RequestMapping("/tag")
 public class TagController {
-    @Autowired
     private TagService tagService;
+    private TagMapper tagMapper;
+
+    public TagController(TagService tagService, TagMapper tagMapper) {
+        this.tagService = tagService;
+        this.tagMapper = tagMapper;
+    }
 
     @GetMapping
     public List<Tag> getAll() {
@@ -23,18 +33,18 @@ public class TagController {
     }
 
     @PostMapping
-    public Tag create(@RequestBody Tag tag) {
-        return this.tagService.create(tag);
+    public TagResponse create(@RequestBody TagRequest tag) {
+        return tagMapper.toResponse(this.tagService.create(tagMapper.toEntity(tag)));
     }
 
     @GetMapping("/{tagId}")
-    public Tag getTagById(@PathVariable String tagId) throws TagNotFoundException {
-        return this.tagService.findById(tagId);
+    public TagResponse getTagById(@PathVariable String tagId) throws TagNotFoundException {
+        return tagMapper.toResponse(this.tagService.findById(tagId));
     }
 
     @PutMapping("/{tadId}")
-    public Tag update(@PathVariable String tadId, @RequestBody Tag tagUpdate) throws TagNotFoundException {
-        return this.tagService.update(tadId, tagUpdate);
+    public TagResponse update(@PathVariable String tadId, @RequestBody TagRequest tagUpdate) throws TagNotFoundException {
+        return tagMapper.toResponse(this.tagService.update(tadId, tagMapper.toEntity(tagUpdate)));
     }
 
     @DeleteMapping("/{tadId}")
