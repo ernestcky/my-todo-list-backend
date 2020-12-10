693fd2773cb6618b7bcdb4f85bcd2e57b57e4bd8
Ernest Cheung <ernestcheung@trustmechain.com>
2020-12-10 21:40:35 +0800

    [TodoIntegrationTest][Ernest] Test: should return all todo when get all given todos

--- src/main/java/com/training/todolist/controller/TodoController.java
+++ src/main/java/com/training/todolist/controller/TodoController.java
@@ -1,9 +1,17 @@
 package com.training.todolist.controller;
 
+import com.training.todolist.entity.Todo;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/Todo")
 public class TodoController {
+    @GetMapping
+    public List<Todo> getAll() {
+        return null;
+    }
 }
---
+++ src/test/java/com/training/todolist/integration/TodoIntegrationTest.java
@@ -0,0 +1,48 @@
+package com.training.todolist.integration;
+
+import com.training.todolist.entity.Tag;
+import com.training.todolist.entity.Todo;
+import com.training.todolist.repository.TodoRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Arrays;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TodoIntegrationTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    private TodoRepository todoRepository;
+
+    @AfterEach
+    void tearDown() {
+        todoRepository.deleteAll();
+    }
+    @Test
+    public void should_return_all_todo_when_get_all_given_todos() throws Exception {
+        //given
+        Todo todo = new Todo("Todo1", false, Arrays.asList(new Tag("tag1", "blue")));
+        todoRepository.save(todo);
+
+        //when
+        //then
+        mockMvc.perform(get("/Todo"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").isString())
+                .andExpect(jsonPath("$[0].text").value("Todo1"))
+                .andExpect(jsonPath("$[0].done").value(false))
+                .andExpect(jsonPath("$[0].tagList[0].content").value("tag1"))
+                .andExpect(jsonPath("$[0].tagList[0].color").value("blue"));
+    }
+}
