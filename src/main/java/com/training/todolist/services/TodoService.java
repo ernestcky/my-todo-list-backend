495e0393b5699eb5fd17714b71b5727247b3188f
Ernest Cheung <ernestcheung@trustmechain.com>
2020-12-10 16:23:25 +0800

    [TodoServiceTest][Ernest] Feat: should return all when get all

--- src/main/java/com/training/todolist/services/TodoService.java
+++ src/main/java/com/training/todolist/services/TodoService.java
@@ -1,13 +1,18 @@
 package com.training.todolist.services;
 
 import com.training.todolist.entity.Todo;
+import com.training.todolist.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class TodoService {
+    @Autowired
+    private TodoRepository todoRepository;
+
     public List<Todo> findAll() {
-        return null;
+        return this.todoRepository.findAll();
     }
 }
